61a3b489accf8892ebd582c59bd8df5c6949f182
SolCasin <150569312+SolCasin@users.noreply.github.com>
2024-07-29 21:35:04 +0200

    Update constants.ts

--- v2-sdk/src/constants.ts
+++ v2-sdk/src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  SEPOLIA = 11155111
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x94f8dcdc11cdf93ca03f09f516f903bf473635e4'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x246ba35fa37fb6bf3b71cd68dd22514e93a5d804976bfd9895b26b4434adb34d'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
